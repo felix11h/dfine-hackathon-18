ce1aa37789a45877cdd09beb0db0fc3e1f722bb4
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-10-20 21:09:11 +0200

    inital -- add all

---
+++ utils/date_time.py
@@ -0,0 +1,4 @@
+
+def yyyy_mm_dd(date):
+    '''convert 180703 to 2018-07-03'''
+    return '20'+date[0:2]+'-'+date[2:4]+'-'+date[4:]
---
+++ utils/legend.py
@@ -0,0 +1,99 @@
+from branca.element import Template, MacroElement
+
+def template_macro(date):
+
+    template = """
+    {% macro html(this, kwargs) %}
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>jQuery UI Draggable - Default functionality</title>
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+      <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+      <script>
+      $( function() {
+        $( "#maplegend" ).draggable({
+                        start: function (event, ui) {
+                            $(this).css({
+                                right: "auto",
+                                top: "auto",
+                                bottom: "auto"
+                            });
+                        }
+                    });
+    });
+
+      </script>
+    </head>
+    <body>
+
+
+    <div id='maplegend' class='maplegend' 
+        style='position: absolute; z-index:9999; border:0px solid grey; background-color:rgba(255, 255, 255, 0.8);
+         border-radius:6px; padding: 20px; font-size:32px; right: 20px; bottom: 20px;'>
+
+    <div class='legend-title'>"""+date+\
+    """</div>
+    <div class='legend-scale'>
+      <ul class='legend-labels'>
+        <li><span style='background:blue;opacity:0.7;'></span> Verkehr</li>
+        <li><span style='background:#E37222;opacity:0.7;'></span>Stickstoffdioxid NO2</li>
+
+      </ul>
+    </div>
+    </div>
+
+    </body>
+    </html>
+
+    <style type='text/css'>
+      .maplegend .legend-title {
+        text-align: left;
+        margin-bottom: 10px;
+        font-weight: bold;
+        font-size: 90%;
+        }
+      .maplegend .legend-scale ul {
+        margin: 0;
+        margin-bottom: 5px;
+        padding: 0;
+        float: left;
+        list-style: none;
+        }
+      .maplegend .legend-scale ul li {
+        font-size: 80%;
+        list-style: none;
+        margin-left: 0;
+        line-height: 18px;
+        margin-bottom: 10px;
+        }
+      .maplegend ul.legend-labels li span {
+        display: block;
+        float: left;
+        height: 16px;
+        width: 30px;
+        margin-right: 5px;
+        margin-left: 0;
+        border: 0px solid #999;
+        }
+      .maplegend .legend-source {
+        font-size: 80%;
+        color: #777;
+        clear: both;
+        }
+      .maplegend a {
+        color: #777;
+        }
+    </style>
+    {% endmacro %}"""
+
+    macro = MacroElement()
+    macro._template = Template(template)
+
+    return macro
